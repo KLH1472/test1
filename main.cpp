34be77447a920b67f777ec1b3b194e965bbe8092
Your Name <you@example.com>
2020-02-13 21:32:03 +0800

    创建a的骨架

---
+++ main.cpp
@@ -0,0 +1,19 @@
+int add(int a, int b){
+    return 0;
+}
+
+int minus(int a, int b){
+    return 0;
+}
+
+int mutiply(int a, int b){
+    return 0;
+}
+
+int divide(int a, int b){
+    return 0;
+}
+
+int power(int a, int b){
+    return 0;
+}
